3261562fe71b1d5d57382764dabb4092ae14a16c
Ayush kamboj <152530498+Ayushh-k@users.noreply.github.com>
2024-09-14 20:32:50 +0530

    Add files via upload

---
+++ traverse_linked_list.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+
+    ~node()
+    {
+        int value = this->data;
+        if (this->next != nullptr)
+        {
+            delete next;
+            next = nullptr;
+        }
+        cout << "Delete node is: " << value << endl;
+    }
+};
+
+void insertAtHead(node *&head, node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        node *temp = new node(data);
+        head = temp;
+        tail = temp;
+    }
+
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(node *&head, node *&tail, int data)
+{
+    if (tail == NULL)
+    {
+        node *temp = new node(data);
+        head = temp;
+        tail = temp;
+    }
+    node *temp = new node(data);
+    tail->next = temp;
+    tail = temp;
+}
+
+void print(node *&head, node *&tail)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node *first = new node(5);
+    node *head = first;
+    node *tail = first;
+    insertAtHead(head, tail, 6);
+    insertAtHead(head, tail, 8);
+    insertAtHead(head, tail, 9);
+    insertAtHead(head, tail, 7);
+
+    insertAtTail(head, tail, 4);
+    insertAtTail(head, tail, 3);
+    insertAtTail(head, tail, 2);
+    insertAtTail(head, tail, 1);
+
+    print(head, tail);
+    return 0;
+}
